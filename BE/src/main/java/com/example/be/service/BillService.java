71c53c547f4b7b0be6e5549fe58f0595cabb1ae4
TruongYNhi <20521720@gm.uit.edu.vn>
2023-07-10 14:04:19 +0700

    update-bill-1

---
+++ BE/src/main/java/com/example/be/service/BillService.java
@@ -0,0 +1,26 @@
+package com.example.be.service;
+
+import com.example.be.model.Bill;
+import com.example.be.model.BillDetails;
+import com.example.be.model.Book;
+import com.example.be.model.User;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface BillService {
+
+    ResponseEntity<Object> getAllBills();
+
+    ResponseEntity<Object> getBillById(Long id);
+
+    List<Book> getBooksByBill(ResponseEntity<Object> bill) throws ChangeSetPersister.NotFoundException;
+
+
+    List<BillDetails> getBillDetailsByBill(Bill bill);
+
+    Bill getActiveBillByUser(User user);
+}
